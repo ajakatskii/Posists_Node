68974fd0303902e993e6b5a06a9591bac2b4c73d
Ankit Jain <ajakatskii@gmail.com>
2017-12-08 00:08:15 +0530

    initial commit, with a minimal file-folder structure

---
+++ PosistNode/App_Code/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosistNode.App_Code
+{
+    /// <summary>
+    /// represents a client node
+    /// </summary>
+    class Node
+    {
+        /// <summary>
+        /// timestamp when the node was created
+        /// </summary>
+        private DateTime _timestamp;
+        /// <summary>
+        /// unique id for the current node, each node in nodechain has a unique id
+        /// </summary>
+        private int _nodeId = -1;
+        /// <summary>
+        /// the set number to which this node belongs
+        /// </summary>
+        private int _nodeSetNumber = -1;
+        /// <summary>
+        /// encrypted data
+        /// </summary>
+        private String _data;
+
+        private Node _parentNode = null;
+
+        private Node _childNode = null;
+
+        private int childNodeId = -1;
+
+    }
+}
---
+++ PosistNode/App_Code/NodeSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosistNode.App_Code
+{
+    /// <summary>
+    /// this manages a complete set of nodes
+    /// </summary>
+    class NodeSet
+    {
+    }
+}
