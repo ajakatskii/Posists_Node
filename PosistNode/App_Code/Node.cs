4a0d6913f9c82bb212b82933d559c560a15994a8
Ankit Jain <ajakatskii@gmail.com>
2017-12-08 04:46:26 +0530

    added node features - get remaining chain lengtth from that node, change passwoord for the given node for tranfer cases etc.

--- PosistNode/App_Code/Node.cs
+++ PosistNode/App_Code/Node.cs
@@ -9,7 +9,7 @@ namespace PosistNode.App_Code
     /// <summary>
     /// represents a client node
     /// </summary>
-    class Node
+    public class Node
     {
         /// <summary>
         /// timestamp when the node was created
@@ -32,7 +32,40 @@ namespace PosistNode.App_Code
 
         private Node _childNode = null;
 
-        private int childNodeId = -1;
+        public Node ChildNode
+        {
+            get
+            {
+                return _childNode;
+            }
+            set
+            {
+                _childNode = value;
+                this.childNodeId = value.Id;
+            }
+        }
+
+        public long Id
+        {
+            get
+            {
+                return this._nodeId;
+            }
+        }
+
+        public Node ParentNode
+        {
+            get
+            {
+                return this._parentNode;
+            }
+            set
+            {
+                this._parentNode = value;
+            }
+        }
+
+        private long childNodeId = -1;
 
         private CipherBox _cipher;
 
@@ -74,6 +107,53 @@ namespace PosistNode.App_Code
             set
             {
                 this._nodeSetNumber = value;
+            }
+        }
+
+        /// <summary>
+        /// when switching owner of the nodes, the new owner's password is supposed to unlock this node.
+        /// </summary>
+        /// <param name="newPassword"></param>
+        /// <returns></returns>
+        public bool ChangePassword(string newPassword)
+        {
+            Decrypter decrypter = null;
+            Encrypter encrypter = null;
+            try
+            {
+                //decrypt the data, then create a new encrypter, with the new password
+                decrypter = new Decrypter(this.Cipher, this._data);
+                encrypter = new Encrypter(newPassword);
+                foreach (KeyValuePair<String, String> kv in decrypter.Data)
+                {
+                    encrypter.AddDataPair(kv.Key, kv.Value);
+                }
+            }catch(Exception e)
+            {
+                //TODO - LOG
+                return false;
+            }
+            this._cipher = encrypter.Cipher;
+            this._data = encrypter.EncryptedData;
+            return true;
+        }
+
+        public int ChainLength
+        {
+            get
+            {
+                if(this._childNode == null)
+                {
+                    return 1;
+                }
+                int count = 0;
+                Node tranverser = this;
+                while (tranverser != null)
+                {
+                    count++;
+                    tranverser = tranverser.ChildNode;
+                }
+                return count;
             }
         }
 
